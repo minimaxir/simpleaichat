b7118c3426caf3d9cc9b11ff19f661ae9f3a686b
Max Woolf <max@minimaxir.com>
2023-05-07 21:45:17 -0700

    refactor to make more composeable

--- utils.py
+++ utils.py
@@ -1,30 +1,40 @@
 import httpx
+from typing import List, Union
+
+WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php"
 
 
-def wikipedia_lookup_closest(query):
-    API_URL = "https://en.wikipedia.org/w/api.php"
+def wikipedia_search(query: str, n: int = 1) -> Union[str, List[str]]:
     SEARCH_PARAMS = {
         "action": "query",
         "list": "search",
         "format": "json",
-        "srlimit": "1",
+        "srlimit": n,
+        "srsearch": query,
         "srprop": "",
     }
 
+    r_search = httpx.get(WIKIPEDIA_API_URL, params=SEARCH_PARAMS)
+    results = [x["title"] for x in r_search.json()["query"]["search"]]
+
+    return results[0] if n == 1 else results
+
+
+def wikipedia_lookup(query: str) -> str:
     LOOKUP_PARAMS = {
         "action": "query",
         "prop": "extracts",
-        "exsentences": "3",
+        "exsentences": "2",
         "exlimit": "1",
         "explaintext": "1",
         "formatversion": "2",
         "format": "json",
+        "titles": query,
     }
 
-    search_params = dict(SEARCH_PARAMS, srsearch=query)
-    r_search = httpx.get(API_URL, params=search_params)
-    lookup_query = r_search.json()["query"]["search"][0]["title"]
-
-    lookup_params = dict(LOOKUP_PARAMS, titles=lookup_query)
-    r_lookup = httpx.get(API_URL, params=lookup_params)
+    r_lookup = httpx.get(WIKIPEDIA_API_URL, params=LOOKUP_PARAMS)
     return r_lookup.json()["query"]["pages"][0]["extract"]
+
+
+def wikipedia_search_lookup(query: str) -> str:
+    return wikipedia_lookup(wikipedia_search(query, 1))
