d928bb7d49b62a382c6295ef40e63d383134a615
Max Woolf <max@minimaxir.com>
2023-06-08 21:05:59 -0700

    add missing dateutil dep #1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="simpleaichat",
     packages=["simpleaichat"],  # this must be the same as the name above
-    version="0.1.0",
+    version="0.1.1",
     description="A Python package for easily interfacing with chat apps, with robust features and minimal code complexity.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -24,5 +24,6 @@ setup(
         "python-dotenv>=1.0.0",
         "orjson>=3.9.0",
         "rich>=13.4.1",
+        "python-dateutil>=2.8.2",
     ],
 )
